8bf5ce8ccc63b23fb48249c46242fa48caea0c5a
Levi Trejo <levi.b.trejo@gmail.com>
2020-11-25 10:40:12 -0600

    feat: exercise 1 bonus

--- src/main/java/com/codeup/blogspring/controllers/RollController.java
+++ src/main/java/com/codeup/blogspring/controllers/RollController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class RollController {
 
@@ -18,6 +21,8 @@ public class RollController {
     public String result(@PathVariable int guess, Model model) {
         int userGuess = guess;
         int roll = rollDie.roll6();
+        List<Integer> rollList = rollDie.rollArr();
+        List<String[]> resultList = new ArrayList<>();
 
         if(userGuess == roll) {
             model.addAttribute("correct", true);
@@ -25,8 +30,22 @@ public class RollController {
             model.addAttribute("correct", false);
         }
 
+        for (int rollGuess : rollList) {
+            String [] temp = new String[2];
+            if (rollGuess == userGuess) {
+                temp[0] = String.valueOf(rollGuess) ;
+                temp[1] = "Y";
+            } else {
+                temp[0] = String.valueOf(rollGuess) ;
+                temp[1] = "N";
+            }
+            resultList.add(temp);
+        }
+
+        model.addAttribute("resultList", resultList);
         model.addAttribute("guess", userGuess);
         model.addAttribute("roll", roll);
+        model.addAttribute("rollList", rollList);
 
         return "rollresult";
     }
