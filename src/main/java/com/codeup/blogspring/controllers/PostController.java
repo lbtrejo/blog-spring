cb4f5f5da9dfd9a9515d79a0fc45c84b1ce92d5d
Levi Trejo <levi.b.trejo@gmail.com>
2020-11-30 10:26:57 -0600

    feat: postcontroller test data, views functional with data

--- src/main/java/com/codeup/blogspring/controllers/PostController.java
+++ src/main/java/com/codeup/blogspring/controllers/PostController.java
@@ -1,5 +1,6 @@
 package com.codeup.blogspring.controllers;
 
+import com.codeup.blogspring.models.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,16 +8,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    public String getPosts() {
+    public String getPosts(Model model) {
+        List<Post> listings = new ArrayList<>();
+        Post test = new Post("Test Title", "Test Body");
+        Post test2 = new Post("Test Title 2", "Test Body 2");
+        listings.add(test);
+        listings.add(test2);
+
+        model.addAttribute("listings", listings);
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
     public String showPost(@PathVariable String id, Model model) {
+        Post test = new Post("Test Title", "Test Body");
+
+        model.addAttribute("post", test);
         model.addAttribute("id", id);
         return "posts/show";
     }
