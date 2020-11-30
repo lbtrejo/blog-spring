6c4c82c5f03bf176a31ffe089a0087f5186e8a67
Levi Trejo <levi.b.trejo@gmail.com>
2020-11-30 10:11:56 -0600

    feat: post views added, controller functional

--- src/main/java/com/codeup/blogspring/controllers/PostController.java
+++ src/main/java/com/codeup/blogspring/controllers/PostController.java
@@ -1,6 +1,7 @@
 package com.codeup.blogspring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,25 +11,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
     public String getPosts() {
-        return "Listing of all posts";
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String showPost(@PathVariable String id) {
-        return String.format("Post ID: %s", id);
+    public String showPost(@PathVariable String id, Model model) {
+        model.addAttribute("id", id);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
-    @ResponseBody
     public String viewCreatePost() {
         return "Create a post!";
     }
 
     @PostMapping("/posts/create")
-    @ResponseBody
     public String createPost() {
         return "Post created";
     }
